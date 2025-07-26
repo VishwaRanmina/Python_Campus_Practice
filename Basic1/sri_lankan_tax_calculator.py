6ed49acf95c1c87b2a1fa6efab8214d78cfb6924
VishwaRanmina <vishwaranmina25@gmail.com>
2025-07-26 15:49:19 +0530

    my 8th commit

--- Basic1/sri_lankan_tax_calculator.py
+++ Basic1/sri_lankan_tax_calculator.py
@@ -1,9 +1,9 @@
 while True:
-    salary = float(input("Enter Your Salary : "))*12
+    salary = float(input("Enter Your Mothly Salary : "))*12
 
     if(salary <= 1200000):
         tax = 0
-        print("YOu Don't need to pay tax")
+        print("YOu Don't need to pay Tax..")
     else:
         if(salary <= 1700000):
             tax = salary - 1200000 * 0.06
@@ -23,5 +23,5 @@ while True:
         else:
             tax = 500000 * 0.06 + 500000 * 0.18 + 500000 * 0.12 + 500000 * 0.24 + 500000 * 0.30 + (salary - 3700000) * 0.36
 
-        print("Monthly Tax : ",tax/12)
-        print("Annual Tax : ",tax)
+        print("Your Monthly Tax : ",tax/12)
+        print("Your Annual Tax : ",tax)
