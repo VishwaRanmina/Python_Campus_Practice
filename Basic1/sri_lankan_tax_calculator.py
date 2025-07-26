da2ea3d10147da69d08daffebbf2ba17d8a141fc
VishwaRanmina <vishwaranmina25@gmail.com>
2025-07-26 15:41:05 +0530

    my 8th commit

--- Basic1/sri_lankan_tax_calculator.py
+++ Basic1/sri_lankan_tax_calculator.py
@@ -1,35 +1,27 @@
 while True:
-    salary = float(input("Enter Annual Your Salary : "))
+    salary = float(input("Enter Your Salary : "))*12
 
     if(salary <= 1200000):
+        tax = 0
         print("YOu Don't need to pay tax")
     else:
-        taxable = salary - 1200000
-        if(taxable <= 500000):
-            tax = taxable*0.06
-            print("Tax :",tax)
+        if(salary <= 1700000):
+            tax = salary - 1200000 * 0.06
+
+        elif(salary <= 2200000):
+            tax = 500000 * 0.06 + salary - 1700000 * 0.12
+
+        elif(salary <= 2700000):
+            tax = 500000 * 0.06 + 500000 * 0.12 + (salary - 2200000) * 0.18
+
+        elif(salary <= 3200000):
+            tax = 500000 * 0.06 + 500000 * 0.18 + 500000*  0.12 + (salary - 2700000) * 0.24
             
-        elif(taxable <= 1000000):
-            tax = taxable * 0.06 + (taxable-500000)*0.12
-            print("Tax :",tax)
-        elif(taxable <= 1500000):
-            tax = 500000 * 0.06 + 500000 * 0.12 + (taxable-500000)*0.18
-            print("Tax :",tax)
-        elif(taxable <= 2000000):
-            tax = 500000 * 0.06 + 500000 * 0.12 + 500000*0.18 + (taxable-500000)*0.24
-            print("Tax :",tax)
-        elif(taxable <= 2500000):
-            tax = 500000 * 0.06 + 500000 * 0.12 + 500000*0.18 + 500000*0.24 + (taxable-500000)*0.30
-            print("Tax :",tax)
-        elif(taxable <= 2500000):
-            tax = 500000 * 0.06 + 500000 * 0.12 + 500000*0.18 +  500000*0.24 + 500000*0.30 + (taxable-500000)*0.36
-            print("Tax :",tax)
-        elif(taxable <= 3000000):
-            tax = 500000 * 0.06 + 500000 * 0.12 + 500000*0.18 +  500000*0.24 + 500000*0.30 + 500000*0.36 + (taxable-500000)*0.36
-            print("Tax :",tax)
+        elif(salary <= 3700000):
+            tax = 500000 * 0.06 + 500000 * 0.18 + 500000 * 0.12 + 500000 * 0.24 + (salary - 3200000) * 0.30
+            
         else:
-            tax = 500000 * 0.06 + 500000 * 0.12 + 500000*0.18 +  500000*0.24 + 500000*0.30 + 500000*0.36 + (taxable-500000)*0.36
-            print("Tax :",tax)
-        
-        
-        
+            tax = 500000 * 0.06 + 500000 * 0.18 + 500000 * 0.12 + 500000 * 0.24 + 500000 * 0.30 + (salary - 3700000) * 0.36
+
+        print("Monthly Tax : ",tax/12)
+        print("Annual Tax : ",tax)
