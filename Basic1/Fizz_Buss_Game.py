1d8bf6fc743f59d163bb1a1e39e594d6d8b67503
VishwaRanmina <vishwaranmina25@gmail.com>
2025-07-26 17:06:49 +0530

    my 11th commit

--- Basic1/Fizz_Buss_Game.py
+++ Basic1/Fizz_Buss_Game.py
@@ -4,10 +4,12 @@ while (num <= 100):
     if (num % 5 == 0 and num % 3 == 0):
         print("FizzBuss")
     
-    if(num % 5 == 0):
+    elif(num % 5 == 0):
         print("Buss")
-    if(num % 3 == 0):
+        
+    elif(num % 3 == 0):
         print("Fizz")
+        
     else:
         print(num)
         
